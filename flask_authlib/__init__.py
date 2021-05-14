6f421d46d60b0616c602815d7a6b16d7f0c090db
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-05-14 19:56:54 +0500

    :information_source: additional info

--- flask_authlib/__init__.py
+++ flask_authlib/__init__.py
@@ -1,4 +1,13 @@
 from .main import Auth
 
 __all__ = [Auth]
-__version__ = "1.0.0"
+
+__license__ = 'MIT'
+__title__ = 'Flask-Authlib'
+__author__ = 'Abduaziz Ziyodov'
+__version_info__ = ('1', '0', '0')
+__version__ = '.'.join(__version_info__)
+__author_email__ = 'abduaziz.ziyodov@mail.ru'
+__copyright__ = '(c) 2021 by Abduaziz Ziyodov'
+__url__ = 'https://github.com/AbduazizZiyodov/flask-authlib'
+__description__ = 'Authentication library for Flask Web Framework '
