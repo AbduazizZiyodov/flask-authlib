21969843f4fb8982acf54ba3e9ab64714189192d
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-05-23 01:28:03 +0500

    🎉🎉 v`1.3.1`

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ clear
 pip uninstall flask_authlib -y
 rm -rf flask_authlib.egg-info/ && rm -rf dist/ && rm -rf build/
 python setup.py sdist bdist_wheel && cd dist/
-pip install flask_authlib-1.2.0-py3-none-any.whl
+pip install flask_authlib-1.3.1-py3-none-any.whl
--- flask_authlib/__init__.py
+++ flask_authlib/__init__.py
@@ -1,12 +1,12 @@
 from .main import Auth
-from .auth0.jwt import JWT
+from .jwt import JWT
 
 __all__ = [Auth, JWT]
 
 __license__ = 'MIT'
 __title__ = 'Flask-Authlib'
 __author__ = 'Abduaziz Ziyodov'
-__version_info__ = ('1', '3', '0')
+__version_info__ = ('1', '3', '1')
 __version__ = '.'.join(__version_info__)
 __author_email__ = 'abduaziz.ziyodov@mail.ru'
 __copyright__ = '(c) 2021 by Abduaziz Ziyodov'
