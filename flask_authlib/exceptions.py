f006ec25f6462f979f6498434ea87cec1a89c60a
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-05-23 01:16:36 +0500

    :+1: add: `AuthError` exception

--- flask_authlib/exceptions.py
+++ flask_authlib/exceptions.py
@@ -2,3 +2,9 @@
 class ConfigError(Exception):
     def __init__(self, description):
         self.description = description
+
+# Auth error exception        
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
