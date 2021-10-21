05006946a7ff0b3e9ce1e1c31f023102917de371
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-10-22 01:01:24 +0500

    😎 Added some new utils

--- flask_authlib/utils.py
+++ flask_authlib/utils.py
@@ -1,79 +1,47 @@
-from flask import Blueprint
+from typing import Callable
+
 from flask import request
-from flask import flash
+from flask import redirect
+from flask.views import View
 
-from werkzeug.exceptions import BadRequestKeyError
+from flask_login import current_user
 
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
 
-def get_alerts() -> dict:
-    """
-    Simple function for returnning messages dict.
-    msg = get_alerts()
-    """
-    messages = {
-        "EMAIL_ALERT": "This email already taken!",
-        "USERNAME_ALERT": "This username already taken!",
-        "PASSWORD_LENGTH": "Password must be 8 characters long",
-        "REGISTER_SUCCESS": "Register was successfuly!",
-        "LOGIN_FAIL": "Email or password incorrect!"
-    }
-
-    return messages
+from pydantic import ValidationError
+from pydantic.main import ModelMetaclass
 
 
-def load_template_config() -> dict:
-    """
-    Function for returning template config (login form ...)
-    config = load_template_config()
-    p.s Based on bootstrap classes
-    """
-    config = {
-        "LOGIN_BTN": 'btn-success',
-        "REGISTER_BTN": 'btn-warning',
-        'LOGIN_BTN_TEXT': 'Login',
-        'REGISTER_BTN_TEXT': 'Register',
-        "LOGIN_PAGE_TITLE": 'Login',
-        "REGISTER_PAGE_TITLE": 'Register',
-        'LOGIN_LABEL_USERNAME': 'Username',
-        'LOGIN_LABEL_PASSWORD': 'Password',
-        'REGISTER_LABEL_USERNAME': 'Username',
-        'REGISTER_LABEL_PASSWORD': 'Password',
-        'REGISTER_LABEL_EMAIL': 'Email address'
-    }
-    return config
+bcrypt = Bcrypt()
 
 
-def create_forms(path: str, forms: list, html: str) -> None:
-    # initial value of counter
-    count: int = 0
-    for form in forms:
-        with open(f'{path}/{form}', 'w') as f:
-            # Write into file
-            f.write(html[count])
-            f.close()
-            count += 1
-
-
-def create_blueprint():
-    return Blueprint('auth', __name__)
-
-
-def get_register_data():
+def validate_request_body(schema: ModelMetaclass) -> bool:
     try:
-        email = request.form['email']
-        username = request.form['username']
-        password = request.form['password']
-
-        return email, username, password
-    except BadRequestKeyError:
-        pass
+        return bool(schema.parse_obj(request.form.to_dict()))
+    except ValidationError:
+        return False
 
 
-def get_login_data():
-    try:
-        username = request.form['username']
-        password = request.form['password']
+def initalize_base_view(cls, base_view: View) -> None:
+    base_view.db = cls.db
+    base_view.base_config = cls.base_config
+    base_view.alerts = cls.alerts
+    base_view.template_config = cls.template_config
 
-        return username, password
-    except BadRequestKeyError:
-        pass
+    base_view.User = cls.User
+    base_view.HOME_URL = base_view.base_config.HOME_URL
+    base_view.LOGIN_URL = base_view.base_config.LOGIN_URL
+    base_view.REGISTER_URL = base_view.base_config.REGISTER_URL
+
+
+def is_authenticated(function) -> Callable:
+    def wrapper(*args, **kwargs):
+        if current_user.is_authenticated:
+            return redirect("/")
+        return function(*args, **kwargs)
+    return wrapper
+
+
+def check_table_name(db: SQLAlchemy, table_name: str) -> bool:
+    return table_name in db.engine.table_names()
