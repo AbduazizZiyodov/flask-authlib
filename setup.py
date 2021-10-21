dd43a68ba00c9fd453de2b02db2c85f48402b9be
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-10-22 00:59:11 +0500

    🎈 Some informations about library are moved to another file

--- setup.py
+++ setup.py
@@ -3,37 +3,29 @@ from os import path
 from setuptools import setup
 from setuptools import find_packages
 
-from flask_authlib import (
-    __author__, __author_email__, __copyright__, __copyright__,
-    __description__, __license__, __title__, __url__, __version__
-)
+from flask_authlib.info import About
 
-parent_dir = path.abspath(path.dirname(__file__))
 
-with open(path.join(parent_dir, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+parent_dir: str = path.abspath(path.dirname(__file__))
+readme_file: str = path.join(parent_dir, "README.md")
+
+with open(readme_file, encoding="utf-8") as f:
+    long_description: str = f.read()
 
 setup(
-    name=__title__,
-    version=__version__,
-    license=__license__,
-    author=__author__,
-    author_email=__author_email__,
-    url=__url__,
-    description=__description__,
-    platforms='any',
+    name=About.name,
+    version=About.version,
+    license=About.license,
+    author=About.author,
+    author_email=About.author_email,
+    url=About.url,
+    description=About.description,
+    platforms="any",
+    include_package_data=True,
     packages=find_packages(),
-    copyright=__copyright__,
+    copyright=About.copyright,
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    install_requires=["flask", "flask_login", "psycopg2-binary",
-                      "sqlalchemy", "flask_sqlalchemy", "flask_bcrypt", "cryptography==3.3.2", "python-jose", 'jwt'],
-    classifiers=[
-        "License :: OSI Approved :: MIT License",
-        'Operating System :: OS Independent',
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Environment :: Web Environment'
-    ]
+    long_description_content_type="text/markdown",
+    install_requires=About.requires,
+    classifiers=About.classifiers
 )
