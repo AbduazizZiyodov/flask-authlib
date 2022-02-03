eb6ad5866baa38997863f63242752b3869640f05
Abduaziz <abduaziz.ziyodov@mail.ru>
2022-02-04 02:04:36 +0500

    🧨 `user info in jwt `settings implemented on jwt required decorator

--- flask_authlib/__init__.py
+++ flask_authlib/__init__.py
@@ -1,10 +1,18 @@
-from .main import Auth
-from .jwt import JWT
+from .main import AuthManager
+from .jwt import *
 
 from .settings import Alerts
+from .settings import JwtConfig
 from .settings import BaseConfig
 from .settings import TemplateConfig
 
 from .schemas import User
 
-__all__ = ["Auth", "JWT", "Alerts", "BaseConfig", "TemplateConfig", "User"]
+from .database.models import get_user_model
+
+
+__all__ = [
+    "AuthManager", "JWT", "Alerts",
+    "BaseConfig", "TemplateConfig", "User",
+    "get_user_model", "JwtConfig"
+]
--- flask_authlib/jwt/main.py
+++ flask_authlib/jwt/main.py
@@ -1,10 +1,12 @@
+from typing import Any
 from typing import Callable
+from typing import Optional
+
+from inspect import signature
 
 from flask import Flask
 from flask import request
 from flask_sqlalchemy import SQLAlchemy
-
-from werkzeug.datastructures import EnvironHeaders
 
 from ..settings import JwtConfig
 from ..database.models import get_user_model
@@ -22,22 +24,29 @@ from ..utils import get_create_admin_function
 from .exceptions import AuthErrorException
 from .exceptions import auth_error_handler
 
-from ..schemas import User
 
-
-class JWT(object):
+class JWT:
     def __init__(
         self,
         app: Flask,
         db: SQLAlchemy,
-        settings: JwtConfig = JwtConfig
+        settings: JwtConfig = JwtConfig,
+        UserModel: Optional[Any] = None
     ) -> None:
+        """
+        Main `JWT` object which allows you to
+        add JWT authentication features.
+        """
         self.app, self.db = app, db
         self.settings = settings
-        self.User = get_user_model(self.db, "users")
+
+        self.User = get_user_model(
+            self.db, self.settings.TABLENAME
+        ) if UserModel is None else UserModel
+
         self.setup()
 
-    def setup(self):
+    def setup(self) -> None:
         if not check_table_name(
             self.db,
             self.settings.TABLENAME
@@ -45,29 +54,33 @@ class JWT(object):
             self.db.create_all()
 
         self.app.config["SECRET_KEY"] = self.settings.SECRET_KEY
+
         self.app.register_error_handler(AuthErrorException, auth_error_handler)
+
         func = get_create_admin_function(self.db, self.settings)
         add_create_admin_command(self.app, func)
 
         self.setup_url_rules()
 
-    def setup_url_rules(self):
+    def setup_url_rules(self) -> None:
         init_base_jwt_view(self, BaseJwtView)
 
         self.app.add_url_rule(
-            "/register",
+            self.settings.REGISTER_URL,
             view_func=JWTRegister.as_view("jwt_register")
         )
 
         self.app.add_url_rule(
-            "/login",
+            self.settings.LOGIN_URL,
             view_func=JWTLogin.as_view("jwt_login")
         )
 
-    def jwt_required(self, func: Callable):
+    def jwt_required(self, func: Callable) -> None:
+        """
+        Decorator for protecting API endpoints with JWT tokens.
+        """
         def decorator(*args, **kwargs) -> Callable:
-            headers: EnvironHeaders = request.headers
-            authorization: str = headers.get("Authorization")
+            authorization: str = request.headers.get("Authorization")
 
             if not authorization:
                 raise AuthErrorException(
@@ -84,7 +97,18 @@ class JWT(object):
             jwt_token: str = jwt_token[1]
             user_data = decode_jwt(jwt_token, self.settings.SECRET_KEY)
 
-            kwargs["user"] = User(**user_data)
+            if signature(func).parameters.get("user") is not None:
+                if self.settings.USER_INFO_IN_JWT:
+                    kwargs["user"] = self.settings.user_schema(**user_data)
+                else:
+                    user = self.User.filter_by(
+                        id=int(user_data)
+                    ).first()
+
+                    kwargs["user"] = self.settings.user_schema(
+                        **user.to_dict()
+                    )
+
             return func(*args, **kwargs)
 
         return decorator
